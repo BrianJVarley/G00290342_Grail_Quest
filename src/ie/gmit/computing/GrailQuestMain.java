1513451cd62116bbfa88fb8cb8bf36ef28b54669
BrianJVarley <brianvarley9@gmail.com>
2015-03-11 15:52:15 +0000

    March 11 - Parsing xml file succesful

--- src/ie/gmit/computing/GrailQuestMain.java
+++ src/ie/gmit/computing/GrailQuestMain.java
@@ -1,5 +1,6 @@
 package ie.gmit.computing;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,49 +25,13 @@ public class GrailQuestMain {
 	    //Load and Parse the XML document
 	    //document contains the complete XML as a Tree.
 	    Document document = 
-	      builder.parse(
-	        ClassLoader.getSystemResourceAsStream("xml/employee.xml"));
+	      builder.parse(new File("network.xml"));
+	    
+	    NodeList locationName = document.getElementsByTagName("location name");
+	    NodeList locationDescription = document.getElementsByTagName("location description");
+	   
+	    
+	}
 
-	    //List<Employee> empList = new ArrayList<>();
-
-	    //Iterating through the nodes and extracting the data.
-	    NodeList nodeList = document.getDocumentElement().getChildNodes();
-
-	    for (int i = 0; i < nodeList.getLength(); i++) {
-
-	      //We have encountered an <employee> tag.
-	      Node node = nodeList.item(i);
-	      if (node instanceof Element) {
-	        //Employee emp = new Employee();
-	       // emp.id = node.getAttributes().
-	            //getNamedItem("id").getNodeValue();
-
-	        NodeList childNodes = node.getChildNodes();
-	        for (int j = 0; j < childNodes.getLength(); j++) {
-	          Node cNode = childNodes.item(j);
-
-	          //Identifying the child tag of employee encountered. 
-	          if (cNode instanceof Element) {
-	            String content = cNode.getLastChild().
-	                getTextContent().trim();
-	            switch (cNode.getNodeName()) {
-	              case "firstName":
-	               // emp.firstName = content;
-	                break;
-	              case "lastName":
-	               // emp.lastName = content;
-	                break;
-	              case "location":
-	              //  emp.location = content;
-	                break;
-	            }
-	          }
-	        }
-	        //empList.add(emp);
-	      }
-
-	    }
-
-
-	  }
+	   
 }
